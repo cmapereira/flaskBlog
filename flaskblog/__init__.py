608799919bc49141640ae132c65c476e8b455d78
cmapereira <cmapereira@gmail.com>
2019-05-09 20:13:06 -0300

    Complete flask blog

---
+++ flaskblog/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '3f5d907cbf291017497f5a1c4904c212'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Faça o login'
+login_manager.login_message_category = 'danger'
+
+from flaskblog import routes
+
